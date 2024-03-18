5d79c74ca18c7bbf98cf060770240e00433c7a5b
Thomas Riccardi <riccardi.thomas@gmail.com>
2024-03-18 01:25:54 +0100

    first parsing done, yay

--- android_alcodroid_backup_parser/main.py
+++ android_alcodroid_backup_parser/main.py
@@ -1,17 +1,24 @@
-from caterpillar.model import struct
+from caterpillar.shortcuts import struct, BigEndian, this, unpack_file
 from caterpillar.fields import *
 
 from rich import print
 import rich_click as click
 
 
+_package_name = b"org.M.alcodroid"
 
-@struct
+@struct(order=BigEndian)
 class AllDataAlcoDroidBackup:
+    # java DataOutput.writeInt
     version: int32
+    # java DataOutput.writeLong
     date: int64
-    package_name: b"org.M.alcodroid\x00"
-    application_name: CString()
+    # java DataOutput.writeUTF: 2 bytes length, then java modified UTF-8
+    package_name_lenght: Const(len(_package_name), uint16) # b"\x00\x0f"
+    package_name: _package_name # ~magic string
+    # java DataOutput.writeUTF: 2 bytes length, then java modified UTF-8
+    # TODO parse java modified UTF-8: https://docs.oracle.com/javase/8/docs/api/java/io/DataInput.html#modified-utf-8
+    application_name: Prefixed(int16)
     todo: Pass
 
 @struct
